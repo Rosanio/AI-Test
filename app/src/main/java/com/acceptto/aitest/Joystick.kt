3375cf7c4ac4a5db6e8077d64f6ac896dc28086b
Matt Rosanio <matt.rosanio@acceptto.com>
2018-02-13 16:38:03 -0800

    Change joystick to show up wherever you first click on the screen

--- app/src/main/java/com/acceptto/aitest/Joystick.kt
+++ app/src/main/java/com/acceptto/aitest/Joystick.kt
@@ -3,43 +3,51 @@ package com.acceptto.aitest
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
-import android.util.Log
 
 
 //todo: make it so wherever user clicks on screen is starting point for joystick, disappears on lift finger
 object Joystick {
     private val boundaryRadius = 100f;
-    private val defaultX = Screen.Width.of32(3)
-    private val defaultY = Screen.Height.of8(7)
-    private var x = defaultX
-    private var y = defaultY
-    private var movementAngle: Double = 0.0
+    private var startingX = -1f
+    private var startingY = -1f
+    private var x = startingX
+    private var y = startingY
+    private var isJoystickActice = false
 
     fun update() {
         val clickPosition = Interface.getClickPoisition()
         if(clickPosition.x >= 0) {
-            val deltaX: Double = clickPosition.x.toDouble() - defaultX
-            val deltaY: Double = clickPosition.y.toDouble() - defaultY
-            val distance = Math.sqrt(Math.pow(deltaX, 2.0)+(deltaY*deltaY))
-            movementAngle = Math.atan2(deltaY, deltaX)
-            if(distance >= boundaryRadius) {
-                x = defaultX + boundaryRadius*Math.cos(movementAngle).toFloat()
-                y = defaultY + boundaryRadius*Math.sin(movementAngle).toFloat()
+            if(isJoystickActice) {
+                val deltaX: Double = clickPosition.x.toDouble() - startingX
+                val deltaY: Double = clickPosition.y.toDouble() - startingY
+                val distance = Math.sqrt(Math.pow(deltaX, 2.0)+(deltaY*deltaY))
+                val movementAngle = Math.atan2(deltaY, deltaX)
+                if(distance >= boundaryRadius) {
+                    x = startingX + boundaryRadius*Math.cos(movementAngle).toFloat()
+                    y = startingY + boundaryRadius*Math.sin(movementAngle).toFloat()
+                } else {
+                    x = clickPosition.x
+                    y = clickPosition.y
+                }
             } else {
-                x = clickPosition.x
-                y = clickPosition.y
+                isJoystickActice = true
+                startingX = clickPosition.x
+                startingY = clickPosition.y
+                x = startingX
+                y = startingY
             }
         } else {
-            x = defaultX
-            y = defaultY
+            isJoystickActice = false
         }
     }
 
     fun draw(canvas: Canvas) {
-        val paint = Paint()
-        paint.color = Color.argb(255, 255, 255, 255)
-        canvas.drawCircle(x, y, 50f, paint)
-        paint.color = Color.argb(150, 255, 255, 255)
-        canvas.drawCircle(defaultX, defaultY, boundaryRadius, paint)
+        if(isJoystickActice) {
+            val paint = Paint()
+            paint.color = Color.argb(255, 255, 255, 255)
+            canvas.drawCircle(x, y, 50f, paint)
+            paint.color = Color.argb(150, 255, 255, 255)
+            canvas.drawCircle(startingX, startingY, boundaryRadius, paint)
+        }
     }
 }
